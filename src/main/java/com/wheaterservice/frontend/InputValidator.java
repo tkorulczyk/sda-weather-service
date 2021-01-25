199062ef8bb620ae41a35e8d0631bbf08fafc35e
tkorulczyk <tomasz.korulczyk@gmail.com>
2021-01-25 23:57:09 +0100

    Completed frontend validation commit

--- src/main/java/com/wheaterservice/frontend/InputValidator.java
+++ src/main/java/com/wheaterservice/frontend/InputValidator.java
@@ -6,6 +6,8 @@ import java.util.Scanner;
 public class InputValidator {
 
     private Scanner scanner = new Scanner(System.in);
+    private final String WARNING_ONLY_LETTERS = Color.RED + ">>> Please type letter only with first being Capital! <<<" + Color.RESET;
+    private final String WARNING_COORDINATE_WITHIN_RANGE = Color.RED + ">>> Please type a number from -90 to 90! <<<" + Color.RESET;
 
     int retrievesInteger() {
         while (true) {
@@ -14,31 +16,39 @@ public class InputValidator {
             } catch (InputMismatchException e) {
                 scanner.reset();
                 scanner.nextLine();
-                System.out.println(Colors.RED + " \nPlease enter a number using only the digits 1 to 4");
+                System.out.println(Color.RED + " \nPlease enter a number using only the digits 1 to 4");
             }
         }
     }
 
-    String retrievesString() {
+    String retrieveAndValidateCoordinates() {
         while (true) {  // Validate using regex
-            try {
-                return scanner.next();
-            } catch (InputMismatchException e) {
-                scanner.reset();
-                scanner.nextLine();
+            float fCoordinate;
+            String coordinate = scanner.next();
+            if (coordinate.matches("-?[0-9]{1,2}[,.]?[0-9]{0,4}")) {
+                String coordinateWithDot = coordinate.replace(',', '.');
+                fCoordinate = Float.parseFloat(coordinateWithDot);
+                if (fCoordinate >= -90 && fCoordinate <= 90) {
+                    return coordinate;
+                } else {
+                    System.out.println(WARNING_COORDINATE_WITHIN_RANGE);
+                }
+            } else {
+                System.out.println(WARNING_COORDINATE_WITHIN_RANGE);
             }
         }
     }
 
 
-    String retrieveLatitudeOrLongitude() {
-        while (true) {  // Validate using regex
-            try {
-                return scanner.next();
-            } catch (InputMismatchException e) {
-                scanner.reset();
-                scanner.nextLine();
+    String retrieveAndValidateLocation() {
+        while (true) {
+            String location = scanner.next();
+            if (location.matches("[A-Z][a-zA-Z]+")) {
+                return location;
+            } else {
+                System.out.println(WARNING_ONLY_LETTERS);
             }
+
         }
     }
 }
